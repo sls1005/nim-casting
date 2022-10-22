07a37b386592d863453a3f9d9d22c33177e43b2c
sls1005 <710052@mail.pcsh.ntpc.edu.tw>
2022-10-22 20:01:17 +0800

    replace runnableExamples with doc comments

--- src/casting.nim
+++ src/casting.nim
@@ -11,21 +11,25 @@ proc dynamicCast*[T: pointer | ptr[object]](p: pointer | ptr[object]): T {.impor
   #                                                                    ^ Can be `nil`!
   ## Can only be used on a pointer to an object of a C++ class.
   ## One might have to enable `-frtti` in order to use this `proc`.
-  ## Note: This can return `nil` if failed.
+  ##
+  ## .. note::
+  ##   This can return `nil` if failed.
 proc dynamicCast*[T: var object](p:var object): T {.importcpp: "dynamic_cast<'0>(#)", nodecl.}
-  #runnableExamples:
-  #  import std/with
-  #  from casting import dynamicCast
-  #  type
-  #    A {.inheritable, pure.} = object
-  #      a: cint
-  #    B = object of A
-  #      b: cstring
-  #  var x: B
-  #  with dynamicCast[var A](x):
-  #    a = 1
-  #  assert x.a == 1
+  ## **Example:**
+  ##
+  ## .. code-block::
+  ##   import std/with
+  ##   from casting import dynamicCast
+  ##   type
+  ##     A {.inheritable, pure.} = object
+  ##       a: cint
+  ##     B = object of A
+  ##       b: cstring
+  ##   var x: B
+  ##   with dynamicCast[var A](x):
+  ##     a = 1
+  ##   assert x.a == 1
 proc reinterpretCast*[T: SomeIntegerOrPointer](p: SomeIntegerOrPointer): T {.importcpp: "reinterpret_cast<'0>(#)", nodecl.}
-  # If the input is non-nil, non-zero, then it does not return `nil`.
+  ## If the input is non-nil and non-zero, the result is not `nil`.
 proc constCast*[T](x: T): T {.importcpp: "const_cast<'0>(#)", nodecl.}
   ## Removes the const qualifier of a C variable.
